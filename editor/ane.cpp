7a3b1fba63a3089789f0100374ec2bd2d7b59419
Anton Fletcher <anton.fletcher@netspot.com.au>
2014-04-09 17:22:43 +0930

    Bah - HTML interface time

--- editor/ane.cpp
+++ editor/ane.cpp
@@ -1,8 +1,23 @@
-#include "editor.h"
+#include <QtGui>
+#include <QtWebKit>
+#include <QApplication>
+#include <QWebView>
+#include <QWebFrame>
+#include <QWebElement>
 
-int main(int argc, char *argv[])
-{
-	Editor _editor;
+int main(int argc, char* argv[]) {
+	QApplication app(argc, argv);
+	QWebView *view = new QWebView();
 
-	_editor.Start();
+	view->show();
+
+	QWebPage *page = view->page();
+
+	QWebFrame *frame = page->mainFrame();
+
+	frame->setHtml("<html><head></head><body><h1>Accidental Noise Editor</h1><div id='menu'><ul><li>Options</li></ul></div><div id='toolbox'><ul><li>Perlin</li></ul></div></body></html>");
+
+	QWebElement element = frame->documentElement();
+
+	return app.exec();
 }
